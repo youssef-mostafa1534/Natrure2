33476bf49f776b633b8385a20c5eb4f4f21b6565
Youssef-Tech <80772590+youssef-mostafa1534@users.noreply.github.com>
2022-12-23 15:09:54 +0200

    Updated Entire Repo

---
+++ js/script.js
@@ -0,0 +1,38 @@
+var swiper = new Swiper(".mySwiper", {
+    grabCursor: false,
+    speed: 600,
+    parallax: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    autoplay: {
+        delay: 4500,
+        disableOnInteraction: false,
+    },
+});
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function dropdown() {
+    document.getElementById("firstList").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
